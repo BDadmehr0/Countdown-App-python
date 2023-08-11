687212a1ce0399b7c5625e2b0cfeb6fb58d0af7d
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-12 02:04:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -48,3 +48,21 @@ class CountdownApp:
             self.update_countdown()
         except ValueError:
             self.show_error("Invalid Input", "Please enter a valid number of years.")
+
+    def update_countdown(self):
+        if self.remaining_seconds > 0:
+            remaining_years = self.remaining_seconds // (365 * 24 * 60 * 60)
+            remaining_days = (self.remaining_seconds % (365 * 24 * 60 * 60)) // (24 * 60 * 60)
+            remaining_hours = ((self.remaining_seconds % (365 * 24 * 60 * 60)) % (24 * 60 * 60)) // (60 * 60)
+            remaining_minutes = (((self.remaining_seconds % (365 * 24 * 60 * 60)) % (24 * 60 * 60)) % (60 * 60)) // 60
+            remaining_seconds = (((self.remaining_seconds % (365 * 24 * 60 * 60)) % (24 * 60 * 60)) % (60 * 60)) % 60
+
+            # Place the numbers of years, days, hours, minutes, and seconds on top of each other
+            self.label.configure(text=f"\n{remaining_years} yrs\n\n\n{remaining_days} day\n\n\n{remaining_hours:02} hrs\n\n\n{remaining_minutes:02} min\n\n\n{remaining_seconds:02} sec")
+            self.remaining_seconds -= 1
+            self.root.after(1000, self.update_countdown)
+        else:
+            self.show_info("Countdown Finished", "Countdown has finished!")
+            self.e()
+            self.last_time = None  # Reset the last saved time
+            self.save_last_time()  # Save the last time after countdown finishes
