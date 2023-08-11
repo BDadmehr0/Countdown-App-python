eeaba74aa4c6728ba1d58b98c156191ace345ead
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-12 02:02:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ import random
 
 class CountdownApp:
     def __init__(self, root):
+        self.root = root
+        self.root.title("Countdown App")
+        self.root.resizable(height=False, width=False)
