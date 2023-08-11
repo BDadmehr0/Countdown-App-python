08897c5f7debfb1387ce18cfd557388770b57b4b
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-12 02:02:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ class CountdownApp:
         self.root = root
         self.root.title("Countdown App")
         self.root.resizable(height=False, width=False)
+        
+        pad = 3
+        self._geom = '200x200+0+0'
+        root.geometry("{0}x{1}+0+0".format(
+            root.winfo_screenwidth()-pad, root.winfo_screenheight()-pad))
+        root.bind('<Escape>', self.toggle_geom)
